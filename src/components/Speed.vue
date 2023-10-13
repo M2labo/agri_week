dcf7801a50362b338216fd6e097c0584d6c3d8b3
KoheiNatsume <mecchi624@gmail.com>
2023-10-13 09:49:24 +0900

    gageの変更

--- src/components/Speed.vue
+++ src/components/Speed.vue
@@ -3,7 +3,7 @@
     <h2>速度</h2>
     <svg class="gauge" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 50">
       <circle cx="50" cy="50" r="40" stroke="grey" stroke-width="2" fill="transparent" />
-      <line :transform="`rotate(${speed*30}, 50, 50)`" x1="50" y1="50" x2="0" y2="50" style="stroke:rgb(255,0,0);stroke-width:2" />
+      <line :transform="`rotate(${speed*10}, 50, 50)`" x1="50" y1="50" x2="0" y2="50" style="stroke:rgb(255,0,0);stroke-width:2" />
     </svg>
     <p> {{ speed/10 }} km/h</p>
       
